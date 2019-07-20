d0b474914a7de25c67d1d4cd116e1f39d9aa1db2
Ronald Royce <ruhruhroy@gmail.com>
2019-07-20 12:00:29 -0400

    Initial changes

---
+++ config/cli-config.php
@@ -0,0 +1,41 @@
+<?php
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\ORM\Tools\Setup;
+use Zend\Stdlib\ArrayUtils;
+// replace with file to your own project bootstrap
+require_once 'bootstrap.php';
+
+// replace with mechanism to retrieve EntityManager in your app
+$entityManager = GetEntityManager();
+
+return ConsoleRunner::createHelperSet($entityManager);
+
+function GetEntityManager() {
+
+	$paths = array(__DIR__ . '/../module/Application/');
+
+	$config = Setup::createAnnotationMetadataConfiguration($paths);
+// the connection configuration
+
+	$globalConfig = require __DIR__ . '/../config/autoload/global.php';
+	$localConfig = require __DIR__ . '/../config/autoload/local.php';
+
+	$config = ArrayUtils::merge($globalConfig, $localConfig);
+
+	$isDevMode = false;
+
+	$dbParams = array(
+		'driver' => $config['doctrine']['connection']['orm_default']['driverClass'],
+		'user' => $config['doctrine']['connection']['orm_default']['params']['user'],
+		'password' => $config['doctrine']['connection']['orm_default']['params']['password'],
+		'dbname' => $config['doctrine']['connection']['orm_default']['params']['dbname'],
+	);
+
+	print_r($dbParams);
+
+	$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+	$entityManager = EntityManager::create($dbParams, $config);
+
+	return $entityManager;
+}
