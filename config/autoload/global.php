5cb1ff3bf5ab5ace854d09c7dc2e1dc7b8b83c2e
Ronald Royce <ruhruhroy@gmail.com>
2019-07-20 12:39:18 -0400

    Emptied global.php

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,22 +1,3 @@
 <?php
 return [
-	'commandline' => [
-		'modules' => [
-			0 => 'Application',
-		],
-	],
-	'doctrine' => [
-		'connection' => [
-			'orm_default' => [
-				'driverClass' => pdo_mysql,
-				'params' => [
-					'host' => 'localhost',
-					'port' => '3306',
-					'driverOptions' => [
-						1002 => 'SET NAMES utf8',
-					],
-				],
-			],
-		],
-	],
 ];
